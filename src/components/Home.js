d0b893a34e73dca9c116e3b895077497725d195f
ezeibarra <85361036+ezeibarra@users.noreply.github.com>
2022-02-16 17:39:09 -0300

    Chair Static + Starting Footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,27 @@
 import React from "react";
 import Layout from "./helpComponents/Layout";
-import './styles/Home.css'
+import "./styles/Home.css";
+import silla from "../assets/Sillanegra.png";
+import MenuX from "./helpComponents/MenuX";
 function Home() {
   return (
     <Layout>
-      <div>
-        <div>
-          <p>Michael W.Dreeben</p>
-          <h2>Shell Dining Chair</h2>
-          <div>
+      <div className="grilla">
+        <div className="box_left">
+          <p className="p_text">Michael W.Dreeben</p>
+          <h2 className="h2_text">Shell Dining Chair</h2>
+          <p className="p_description">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s
-          </div>
-          <button>Product Details</button>
+          </p>
+          <button className="button_detail">Product Details</button>
+        </div>
+        <div className="box_right">
+          <img src={silla} alt="Imagen" className="imagen" />
         </div>
       </div>
+      <MenuX/>
     </Layout>
   );
 }
---
+++ src/components/helpComponents/MenuX.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/MenuX.css";
+function MenuX() {
+  return (
+    <div className="grilla_menu_x">
+      <div className="footer">
+        <div className="uno">
+          <p>1</p>
+          <p>Shell Dining Chair</p>
+        </div>
+        <div className="dos">
+          <p>1</p>
+          <p>Shell Dining Chair</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MenuX;
