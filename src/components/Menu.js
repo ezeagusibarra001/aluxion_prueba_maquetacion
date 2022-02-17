a32146e7b0ef617c8ef9d4f7ee206f31b61e0b8e
ezeibarra <85361036+ezeibarra@users.noreply.github.com>
2022-02-17 01:33:48 -0300

    Complete Right Side without animation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "./helpComponents/Layout";
 import "./styles/Menu.css";
-import sillon from "../assets/sillonblanco.png"
+import sillon from "../assets/sillonblanco.png";
 function Menu() {
   const enter = () => {
     document.querySelector(".bellow_list").classList.add("block");
@@ -14,10 +14,10 @@ function Menu() {
 
   const enterSillon = () => {
     document.querySelector("#sillon").classList.add("block");
-  }
+  };
   const leaveSillon = () => {
     document.querySelector("#sillon").classList.remove("block");
-  }
+  };
   return (
     <Layout>
       <div className="grilla_menu">
@@ -25,7 +25,9 @@ function Menu() {
           <div onMouseEnter={enter} onMouseLeave={leave}>
             <li id="collection">Collection</li>
             <ul className="bellow_list">
-              <li onMouseEnter={enterSillon} onMouseLeave={leaveSillon}>Furniture</li>
+              <li onMouseEnter={enterSillon} onMouseLeave={leaveSillon}>
+                Furniture
+              </li>
               <li>Lighting</li>
               <li id="last_one">Accesories</li>
             </ul>
@@ -36,7 +38,17 @@ function Menu() {
           <li>Ethics</li>
         </ul>
         <div id="sillon" className="sillon">
-          <img src={sillon} alt="Sillon"/>
+          <img src={sillon} alt="Sillon" />
+        </div>
+      </div>
+      <div className="keep_down">
+        <div className="grilla_menu2">
+          <div className="group1">
+            <p>About</p> <p>Contact</p> <p>Dealers</p>
+          </div>
+          <div className="group2"> 
+            <p>News</p> <p>Care</p> <p>Press</p>
+          </div>
         </div>
       </div>
     </Layout>
